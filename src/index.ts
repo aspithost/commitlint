31e7ed0d96953f6c1e8d6598d5ddc74316aab487
aspithost <abelspithost@gmail.com>
2026-04-15 22:43:45 +0200

    chore: updates dependencies, version

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { RuleConfigSeverity, type UserConfig } from '@commitlint/types';
-export { COMMIT_TYPES } from './constants/commitTypes.js';
+
 export { configuration, configuration as default, createConfig } from './config/config.js';
+export { COMMIT_TYPES } from './constants/commitTypes.js';
